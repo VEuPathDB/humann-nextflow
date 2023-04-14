861a9588356ba88e5183f5acf39bfee1863a312c
rdemko2332 <rdemko2332@gmail.com>
2023-04-14 16:11:06 -0400

    Adding script for new download functionality for dnaseq

---
+++ bin/getFilesFromSra.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use Getopt::Long;
+
+# Creating Variable
+my ($strain,$idList);
+
+# Creating Arguments
+&GetOptions("strain|i=s" => \$strain,
+            "idList|l=s" => \$idList);
+
+my @idArray = split(/,/, $idList);
+
+foreach my $id (@idArray){
+
+    system("fasterq-dump --split-3 ${id}");
+    if(-e "$strain_1.fastq"){
+        system("cat $id\_1.fastq >> ${strain}_1.fastq");
+    }
+    if(-e "$strain_2.fastq"){
+	system("cat $id\_2.fastq >> ${strain}_2.fastq");
+    }
+    
+}
+
